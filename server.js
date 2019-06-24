f583db247003083f1128c8e4f933757e89521fb3
Rodrigo <gmrodrigoborges@gmail.com>
2019-06-24 16:38:27 -0300

    Server working

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const port = process.env.FPRINT_SERVER_PORT || yargs.port || 4444;
 
 const reader = new FingerprintReader({
 	deviceId		: process.env.FPRINT_SERVER_DEVICE || yargs.device || 'Digital Persona U.are.U 4000/4000B/4500',
-	settingsPath	: process.env.FPRINT_SERVER_CONFPATH || yargs.db || './db.json',
 	debug			: true
 })
 	.on('identify', _onIdentify)
@@ -24,70 +23,40 @@ const reader = new FingerprintReader({
 const server = express()
 
 	.use( cors() )
-	.use( bodyParser.json() )
+	.use( bodyParser.urlencoded({extended:true}) )
 
 	.use(( req, res, next ) => {
 
 		res.locals.success = function( message, statusCode ) {
 			return res
 				.status( statusCode || 200 )
-				.json({
-					success: true,
-					message: message
-				})
-		}
-
-		res.locals.error = function( message, statusCode ) {
-
-			if( message instanceof Error )
-				message = message.message || message.toString()
-
-			return res
-				.status( statusCode || 500 )
-				.json({
-					success: false,
-					message: message
-				})
+				.json(message)
 		}
 
 		next();
 
 	})
 
-	.get( '/api/user/:id', ( req, res ) => {
-
-		let user = reader.getUser( req.params.id, ( req.query.fingerprints === '1' ) );
-		if( user instanceof Error ) return res.locals.error( user );
-		return res.locals.success( user );
-
-	})
-	.get( '/api/user', ( req, res ) => {
-
-		let users = reader.getUsers( ( req.query.fingerprints === '1' ) );
-		if( users instanceof Error ) return res.locals.error( users );
-		return res.locals.success( users );
-
-	})
-	.put( '/api/user/:id', ( req, res ) => {
-
-		let user = reader.updateUser( req.params.id, req.body );
-		if( user instanceof Error ) return res.locals.error( user );
-		return res.locals.success( user );
-
-	})
-	.post( '/api/user', ( req, res ) => {
-
-		reader.addUser( req.body, ( err, result ) => {
-			if( err ) return res.locals.error( err );
+	.get( '/api/start', ( req, res ) => {
+		var result = reader._startIdentifyServer( result => {
 			return res.locals.success( result );
 		});
 
 	})
-	.delete( '/api/user/:id', ( req, res ) => {
 
-		let user = reader.deleteUser( req.params.id );
-		if( user instanceof Error ) return res.locals.error( user );
-		return res.locals.success( user );
+	.post( '/api/biometry', ( req, res ) => {
+
+		reader._addBiometry( req.body, ( err, result ) => {
+			return res.locals.success( result );
+		});
+
+	})
+
+	.get( '/api/users', ( req, res ) => {
+		var page = typeof req.query.page !== 'undefined' ? req.query.page : 1
+		var result = reader._getUsers( page, result => {
+			return res.locals.success( result );
+		});
 
 	})
 
