4022f7e577aa2aa9bcbcf7af056730785a874068
Rodrigo <gmrodrigoborges@gmail.com>
2019-11-03 11:28:50 -0300

    removendo código

--- server.js
+++ server.js
@@ -19,63 +19,23 @@ const server = require('http').createServer(app)
 const socket = require('socket.io')
 const io = socket(server)
 
+app.use( cors() )
+app.use( bodyParser.urlencoded({extended:true}) )
 
-	app.use( cors() )
-	app.use( bodyParser.urlencoded({extended:true}) )
+io.on('connection', function(connection){
 
-	// .use(( req, res, next ) => {
-	// 	res.locals.success = function( message, statusCode ) {
-	// 		return res
-	// 			.status( statusCode || 200 )
-	// 			.json(message)
-	// 	}
-
-	// 	next();
-
-	// })
-
-	// .get( '/api/start', ( req, res ) => {
-	// 	var result = reader._startIdentifyServer( result => {
-	// 		return res.locals.success( result );
-	// 	});
-
-	// })
-
-	// .get( '/api/biometry/:id', ( req, res ) => {
-	// 	var result = reader._addBiometry( req.params.id, result => {
-	// 		return res.locals.success( result );
-	// 	});
-
-	// })
-
-	// .get( '/api/users', ( req, res ) => {
-	// 	var page = typeof req.query.page !== 'undefined' ? req.query.page : 1
-	// 	var result = reader._getUsers( page, result => {
-	// 		return res.locals.success( result );
-	// 	});
-
-	// })
-
-	// .listen( port, () => {
-	// 	console.log(`fprint server running on port ${port}...`);
-	// });
-
-	io.on('connection', function(connection){
-
-		connection.on('identify', data => {
-			reader._startIdentifyServer( connection )
-		})
-
-		connection.on('users', page => {
-			reader._getUsers( connection, page )
-		})
-
-		connection.on('addBiometry', id => {
-			reader._addBiometry( connection, id )
-		})
-
-
+	connection.on('identify', data => {
+		reader._startIdentifyServer( connection )
 	})
 
+	connection.on('users', page => {
+		reader._getUsers( connection, page )
+	})
 
-	server.listen(port)
+	connection.on('addBiometry', id => {
+		reader._addBiometry( connection, id )
+	})
+
+})
+
+server.listen(port)
